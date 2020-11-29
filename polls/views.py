ad8f0fe7e64c4d698b8d4b39d54ff87cb2cc053d
foday <foday_s@yahoo.com>
2020-11-29 14:06:11 -0600

    wrote path from niews to index function in the urls.py file then wrote the index function and add os.path.join(BASE_DIR, 'templates')] to setting file to make templates universal

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
+#def index(request):
+#    return HttpResponse("Hello. You're at the polls index")
+
 def index(request):
-    return HttpResponse("Hello. You're at the polls index")
+    return render(request, 'polls/index.html')
